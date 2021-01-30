59009bfe141e5f501a17455718a1c0e5d35a62a9
FonderElite <71190959+FonderElite@users.noreply.github.com>
2021-01-31 00:53:18 +0800

    Create client.py

---
+++ client.py
@@ -0,0 +1,7 @@
+from vidstream import CameraClient
+from vidstream import VideoClient
+from vidstream import ScreenShareClient
+
+# Choose One
+client1 = CameraClient('127.0.0.1', 9999)
+client1.start_stream()
